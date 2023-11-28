edaa96a77dfc375aef613bed511eca3b3bde609a
cyfinfaza <cyfinfaza@gmail.com>
2023-11-28 13:44:29 -0500

    basic functionality working

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from ru_cptdb_scraper import scrapeByNum
 import json
 
@@ -6,11 +6,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, world!"
+    return render_template("index.html")
 
 @app.route("/bus/<int:bus_num>")
 def bus_page(bus_num):
-    return scrapeByNum(bus_num)
+    bus_data = scrapeByNum(bus_num)
+    return render_template("bus.html", **bus_data)
 
 @app.route("/api/bus/<int:bus_num>")
 def api_bus_page(bus_num):
